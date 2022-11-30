57e104e5bd1db4effe022cca7ec948a5ed6fc942
Almaz Choiubekov <choiubekov.almaz@gmail.com>
2022-11-30 21:09:20 +0600

    Added basic layout Added Navbar Added Home, Add, About, Contacts conts Added these comps to App with router

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,26 @@
+import { Routes, Route } from "react-router-dom";
+import Navbar from "./components/Navbar/Navbar";
+import About from "./containers/About/About";
+import Add from "./containers/Add/Add";
+import Contacts from "./containers/Contacts/Contacts";
+import Home from "./containers/Home/Home";
+
 function App() {
-  return <></>;
+  return (
+    <>
+      <header>
+        <Navbar />
+      </header>
+      <main className="container">
+        <Routes>
+          <Route path='/' element={<Home/>}/>
+          <Route path='/add' element={<Add/>}/>
+          <Route path='/about' element={<About/>}/>
+          <Route path='/contacts' element={<Contacts/>}/>
+        </Routes>
+      </main>
+    </>
+  );
 }
 
 export default App;
