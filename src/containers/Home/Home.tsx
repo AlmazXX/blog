cb360fc7dcc1cea2f6619b85270bcab4d04b5955
Almaz Choiubekov <choiubekov.almaz@gmail.com>
2022-12-01 00:18:35 +0600

    Added Posts comp to Home

--- src/containers/Home/Home.tsx
+++ src/containers/Home/Home.tsx
@@ -1,5 +1,37 @@
+import { useCallback, useEffect, useState } from "react";
+import axiosApi from "../../axiosApi";
+import Posts from "../../components/Posts/Posts";
+import Spinner from "../../components/Spinner/Spinner";
+import { IPost, IPostsList } from "../../types";
+
 const Home = () => {
-  return <div>Home</div>;
+  const [posts, setPosts] = useState<IPost[]>([]);
+  const [loading, setLoading] = useState(false);
+
+  const fetchPosts = useCallback(async () => {
+    try {
+      setLoading(true);
+      const postsResponse = await axiosApi.get<IPostsList>("/posts.json");
+      const posts = Object.keys(postsResponse.data).map((key) => ({
+        ...postsResponse.data[key],
+        id: key,
+      }));
+      setPosts(posts);
+    } finally {
+      setLoading(false);
+    }
+  }, []);
+
+  useEffect(() => {
+    void fetchPosts();
+  }, [fetchPosts]);
+
+  return (
+    <div className="mt-3">
+      <h4>My Blog</h4>
+      {loading ? <Spinner /> : <Posts posts={posts} />}
+    </div>
+  );
 };
 
 export default Home;
