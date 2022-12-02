d9fe9c0bb5b56df0c4b0e64cca8352f1cf5302f5
Almaz Choiubekov <choiubekov.almaz@gmail.com>
2022-12-02 16:30:46 +0600

    Created Post comp for PostPage

---
+++ src/components/Post/Post.tsx
@@ -0,0 +1,30 @@
+import { FC, MouseEventHandler } from "react";
+import { Link } from "react-router-dom";
+import { IPost } from "../../types";
+
+interface Props {
+  post: IPost;
+  onDelete: MouseEventHandler;
+}
+
+const Post: FC<Props> = ({ post, onDelete }) => {
+  return (
+    <>
+      <h4>{post.title}</h4>
+      <p className="small">
+        Created on: <span>{post.date.split("T").join(" ")}</span>
+      </p>
+      <p className="bg-light rounded-3 p-3 lh-lg">{post.body}</p>
+      <div className="d-flex gap-3 px-0">
+        <Link to={`/posts/${post.id}/edit`} className="btn btn-primary">
+          Edit post
+        </Link>
+        <button className="btn btn-danger" onClick={onDelete}>
+          Delete post
+        </button>
+      </div>
+    </>
+  );
+};
+
+export default Post;
